2512c9cf2e5ce398c3d8593fd94cc4fe034e3c9d
JaehoJung <jaeho.mundo@gmail.com>
2022-01-16 17:39:10 +0900

    git

---
+++ doWriteRegularly/Model/Note/NoteModel.swift
@@ -0,0 +1,15 @@
+//
+//  NoteModel.swift
+//  doWriteRegularly
+//
+//  Created by Jaeho Jung on 2022/01/16.
+//
+
+import Foundation
+
+//2
+//class NoteModel {
+//    var id: String!
+//    var title = ""
+//    var content = ""
+//}
